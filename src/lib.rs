7fb846635662acaf602dc4eb4aec4ddefb7dcbcf
Mipmipp <sebastianhernandoh@gmail.com>
2024-02-01 13:03:47 -0300

    mend

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub fn calculate_bmi(weight: f64, height: f64) -> f64 {
-    weight / (height * height)
-} 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
- 
+mod errors;
+mod messages;
+mod utils;
+use std::io;
+
 fn main() {
-     println!("Hello, world!");
+    let mut weight = String::new();
+    let mut height = String::new();
+
+    println!("{}", messages::ENTER_WEIGHT);
+    io::stdin()
+        .read_line(&mut weight)
+        .expect(errors::ERROR_READ_WEIGHT);
+
+    println!("{}", messages::ENTER_HEIGHT);
+    io::stdin()
+        .read_line(&mut height)
+        .expect(errors::ERROR_READ_HEIGHT);
+
+   
+
+    let weight_parsed: f64 = utils::parse_input(weight).unwrap()
+    let height_parsed: f64 = utils::parse_input(height).unwrap()
+
+
+    println!("data, {}, {}!", weight_parsed, height_parsed);
 }
