0e2361e9913eb14e52d68e97460943068ce676bf
Mipmipp <sebastianhernandoh@gmail.com>
2024-02-01 13:53:21 -0300

    Fix typo

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
 pub const ERROR_READ_WEIGHT: &str = "Error reading the weight.";
 pub const ERROR_READ_HEIGHT: &str = "Error reading the height.";
 pub const ERROR_VALID_NUMBER: &str = "Please enter a valid number.";
-pub const ERROR_EMPTY_INPUT: &str = "Input is empty Please enter a number.";
+pub const ERROR_EMPTY_INPUT: &str = "Input is empty. Please enter a number.";
